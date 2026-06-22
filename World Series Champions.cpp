c143e6a7746b5f6857ab8b149c959c52ecc66130
Nae-23 <dgray0123@mscc.edu>
2026-06-22 18:52:32 +0000

    Refactor trim function and improve error handling in World Series Champions program; add VSCode tasks configuration

--- World Series Champions.cpp
+++ World Series Champions.cpp
@@ -4,30 +4,45 @@
 
 using namespace std;
 
+string trim(const string &s)
+{
+    size_t start = s.find_first_not_of(" \t\r\n");
+    if (start == string::npos) return "";
+    size_t end = s.find_last_not_of(" \t\r\n");
+    return s.substr(start, end - start + 1);
+}
+
 int main()
 {
-    // Dectermine variables
+    // Determine variables
     ifstream teamsFile("teams.txt");
     ifstream winnersFile("WorldSeriesWinners.txt");
     string teamName;
     string winnerName;
     int count = 0;
 
+    if (!teamsFile || !winnersFile)
+    {
+        cout << "Error opening input files." << endl;
+        return 1;
+    }
+
     // Display all teams from the file
     cout << "Teams that have won the World Series:\n";
     while (getline(teamsFile, teamName))
     {
-        cout << teamName << endl;
+        cout << trim(teamName) << endl;
     }
     
     // Ask user to enter a team name
     cout << "\nEnter the name of a team: ";
     getline(cin, teamName);
+    teamName = trim(teamName);
     
     // Count how many times the team appears in the winners file
     while (getline(winnersFile, winnerName))
     {
-        if (winnerName == teamName)
+        if (trim(winnerName) == teamName)
         {
             count++;
         }
